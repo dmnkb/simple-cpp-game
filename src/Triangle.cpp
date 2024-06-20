2fd4f4c465859c89029dcb9b413baae709591e47
Dominik Borchert <dev@borchert.me>
2024-06-20 12:04:48 +0200

    feat: refactor triangle to it's own class

---
+++ src/Triangle.cpp
@@ -0,0 +1,49 @@
+#include "Triangle.h"
+#include <stdio.h>
+
+Triangle::Triangle() : program(glCreateProgram())
+{
+  GLuint vertex_buffer;
+  glGenBuffers(1, &vertex_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+  glCompileShader(vertex_shader);
+
+  const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+  glCompileShader(fragment_shader);
+
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, fragment_shader);
+  glLinkProgram(program);
+  mvp_location = glGetUniformLocation(program, "MVP");
+  vpos_location = glGetAttribLocation(program, "vPos");
+  vcol_location = glGetAttribLocation(program, "vCol");
+
+  glGenVertexArrays(1, &vertex_array);
+  glBindVertexArray(vertex_array);
+  glEnableVertexAttribArray(vpos_location);
+  glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void *)offsetof(Vertex, pos));
+  glEnableVertexAttribArray(vcol_location);
+  glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                        (void *)offsetof(Vertex, col));
+}
+
+void Triangle::draw(const float ratio)
+{
+
+  mat4x4 m, p, mvp;
+  mat4x4_identity(m);
+  mat4x4_rotate_Z(m, m, (float)glfwGetTime());
+  mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+  mat4x4_mul(mvp, p, m);
+
+  glUseProgram(program);
+  glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *)&mvp);
+  glBindVertexArray(vertex_array);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+}
